584144ccf5a04d390a22ec887b93340ded8f179f
Abhilash <abhilash301267@gmail.com>
2024-03-23 00:31:12 +0530

    dev test

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const TopBar = () => {
+  return (
+    <div className= "text-white p-4">
+      <div className="container mx-auto">
+        <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl text-white">Penguin Labss</h1>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
